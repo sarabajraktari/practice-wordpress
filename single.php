103a30e36d0b93ed202eb3f5e505773c63a2b78a
sarabajraktari <sarabajraktari90@gmail.com>
2022-12-06 11:18:29 +0100

    created single page

---
+++ single.php
@@ -0,0 +1,56 @@
+<?php get_header(); ?>
+
+<div class="row">
+
+    <div class="col-xs-12 col-sm-8">
+
+        <?php 
+		
+		if( have_posts() ):
+			
+			while( have_posts() ): the_post(); ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+            <?php the_title('<h1 class="entry-title">','</h1>' ); ?>
+
+            <?php if( has_post_thumbnail() ): ?>
+
+            <div class="pull-right"><?php the_post_thumbnail('thumbnail'); ?></div>
+
+            <?php endif; ?>
+            <!-- Built in functions -->
+            <small><?php the_category(' '); ?> || <?php the_tags(); ?> || <?php edit_post_link(); ?></small>
+
+            <?php the_content(); ?>
+
+            <hr>
+
+            <?php 
+                            // Boolean function
+						if( comments_open() ){ 
+                            // Built in function
+							comments_template(); 
+						} else {
+							echo '<h5 class="text-center">Sorry, Comments are closed!</h5>';
+						}
+						
+					 ?>
+
+        </article>
+
+        <?php endwhile;
+			
+		endif;
+				
+		?>
+
+    </div>
+
+    <div class="col-xs-12 col-sm-4">
+        <?php get_sidebar(); ?>
+    </div>
+
+</div>
+
+<?php get_footer(); ?>
